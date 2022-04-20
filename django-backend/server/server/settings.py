546180673c68153435560071c78713748f4f564f
Sarwar Saif <sarwar.saif@rakuten.com>
2022-04-21 02:33:33 +0900

    20220421: removed secret key

--- django-backend/server/server/settings.py
+++ django-backend/server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--3*uu$wf3hb^@4(ls)0$dv4@r)ii4*74o-ni5r2bv!+qt#up96'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
